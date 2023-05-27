f99d413fd55e610a556eb7ce4ddb7e1f697e7897
NuckenMcFuggets <cameron.ian.ronald.ord@Outlook.com>
2023-05-27 13:54:12 -0600

    added input and exceptions

---
+++ py3.py
@@ -0,0 +1,15 @@
+try:
+    num = input("Input a number")
+    test = 2 + num
+
+except TypeError:
+    print("you cant mix and match types")
+
+except:
+    print("something has gone wrong")
+
+finally:
+    print("I will run no matter what")
+
+choice = input("Please enter your name:")
+print("Hello", choice)
